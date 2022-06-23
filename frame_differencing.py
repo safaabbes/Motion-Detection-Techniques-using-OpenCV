b5f49f1622e6f278fc3fce312eb013d0177d2b05
Safa Abbes <abbessafa1998@gmail.com>
2022-06-23 10:00:01 +0200

    Update frame_differencing.py

--- frame_differencing.py
+++ frame_differencing.py
@@ -3,28 +3,19 @@ import numpy as np
 
 frames=[]
 MAX_FRAMES = 1000
-N = 30
-THRESH = 50
-ASSIGN_VALUE = 255 ## Value to assign the pixel if the threshold is met
+N = 2
+THRESH = 60
+ASSIGN_VALUE = 255 #Value to assign the pixel if the threshold is met
 
-webcam = False
-
-if webcam:
-    cap = cv2.VideoCapture(0)
-else:
-    cap = cv2.VideoCapture("material/Video.mp4")
+cap = cv2.VideoCapture(0)  #Capture using Computer's Webcam
     
 for t in range(MAX_FRAMES):
     #Capture frame by frame
     ret, frame = cap.read()
-    #When Video reaches its end
-    if not ret:
-        break
     #Convert frame to grayscale
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) 
     #Append to list of frames
     frames.append(frame_gray)
-    
     if t >= N:
         #D(N) = || I(t) - I(t+N) || = || I(t-N) - I(t) ||
         diff = cv2.absdiff(frames[t-N], frames[t])
@@ -33,17 +24,12 @@ for t in range(MAX_FRAMES):
         ret, motion_mask = cv2.threshold(diff, THRESH, ASSIGN_VALUE, threshold_method)
         #Display the Motion Mask
         cv2.imshow('Motion Mask', motion_mask)
-    
-    #Display the resulting frame
-    cv2.imshow('Frame', frame)
     #Wait and exit if q is pressed
-    if cv2.waitKey(1) == ord('q') or not ret:
+    if cv2.waitKey(1) == ord('e') or not ret:
         break
 
 #When everything is finished, we release the capture
 cap.release()
 cv2.destroyAllWindows()
-        
-
     
 
